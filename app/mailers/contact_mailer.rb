5f8c88b4be242b4cd71e3c53c860b5645b0d5d17
Wesley Day <wesleyday@hotmail.com>
2017-04-01 17:46:34 -0500

    fixing contact form

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -1,10 +1,10 @@
 class ContactMailer < ActionMailer::Base
-  default from: "byoc.ninja <webmaster@byoc.ninja>"
-  default to: "byoc.ninja <#{ENV["EMAIL_USERNAME"]}>"
+  default from: "byoc ninja <#{ENV["EMAIL_USERNAME"]}>"
+  default to: "byoc ninja <#{ENV["EMAIL_USERNAME"]}>"
 
   def new(message)
     @message = message
-    
+
     mail from: "#{message.name} <#{message.email}>"
     mail subject: "BYOC.NINJA >> Message from #{message.name} <#{message.email}>"
   end
