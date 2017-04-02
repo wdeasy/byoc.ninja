6d74004db445a759f598acb6a1b3beb0bee462af
Wesley Day <wesleyday@hotmail.com>
2017-04-01 23:40:25 -0500

    fixing contact form

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,4 +77,19 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
+  config.action_mailer.default_url_options = { :host => ENV["EMAIL_DOMAIN"] }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.default :charset => "utf-8"
+  config.action_mailer.smtp_settings = {
+    address: ENV["SMTP_SERVER"],
+    port: 587,
+    domain: ENV["EMAIL_DOMAIN"],
+    user_name: ENV["EMAIL_USERNAME"],
+    password: ENV["EMAIL_PASSWORD"],
+    authentication: :login,
+    enable_starttls_auto: true
+  }
+
 end
