eeea6898f1cf7aa72e6d84384a1568d99828b6a3
Wesley Day <wesleyday@hotmail.com>
2017-04-01 23:27:11 -0500

    fixing contact form

--- config/application.rb
+++ config/application.rb
@@ -24,6 +24,8 @@ module ServerBrowser
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
 
+    config.action_mailer.default_url_options = { :host => ENV["EMAIL_DOMAIN"] }
+
     config.action_mailer.delivery_method = :smtp
     config.action_mailer.perform_deliveries = true
     config.action_mailer.raise_delivery_errors = false
