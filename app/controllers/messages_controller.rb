2ba07f93bc73a6771ee214032594550fed0d0dba
Wesley Day <wesleyday@hotmail.com>
2018-07-10 22:10:17 -0500

    redirect_to :back is deprecated in rails5.

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -6,7 +6,7 @@ class MessagesController < ApplicationController
     ids = [params[:id], *cookies.signed[:hidden_message_ids]]
     cookies.permanent.signed[:hidden_message_ids] = ids
     respond_to do |format|
-      format.html { redirect_to :back }
+      format.html { redirect_back(fallback_location: root_path) }
       format.js
     end
   end
@@ -77,5 +77,5 @@ class MessagesController < ApplicationController
     # Confirms an admin user.
     def admin_user
       redirect_to(root_url) unless current_user.admin?
-    end    
+    end
 end
