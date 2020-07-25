c9b5d543014bb1eb6878566ff42141c1aa051d88
Wesley Day <wesleyday@hotmail.com>
2020-07-24 21:56:31 -0500

    revert csp

--- config/initializers/content_security_policy.rb
+++
@@ -1,8 +0,0 @@
-Rails.application.config.content_security_policy do |policy|
-  policy.default_src :self, :https
-  policy.font_src    :self, :https, :data
-  policy.img_src     :self, :https, :data
-  policy.object_src  :none
-  policy.script_src  :self, :https
-  policy.style_src   :self, :https, :unsafe_inline
-end
