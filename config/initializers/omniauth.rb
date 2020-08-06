42ad8de08b1dde3884643b979b40f4c073cb757b
Wesley Day <wesleyday@hotmail.com>
2020-08-06 10:17:24 -0500

    discord oauth prompt

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,6 +1,6 @@
 # config/initializers/omniauth.rb
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :discord, ENV['DISCORD_CLIENT_ID'], ENV['DISCORD_CLIENT_SECRET'], scope: 'identify connections'
+  provider :discord, ENV['DISCORD_CLIENT_ID'], ENV['DISCORD_CLIENT_SECRET'], scope: 'identify connections', prompt: 'none'
   provider :steam, ENV["STEAM_WEB_API_KEY"]
   provider :bnet, ENV['BNET_KEY'], ENV['BNET_SECRET']
   provider :twitch, ENV["TWITCH_CLIENT_ID"], ENV["TWITCH_CLIENT_SECRET"]
