50fc0e6dfbe27bf91e5bbb81092f1b83bf80b64f
Wesley Day <wesleyday@hotmail.com>
2023-05-25 19:00:51 -0500

    updating dependencies

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_24_045303) do
-
+ActiveRecord::Schema[7.0].define(version: 2021_08_24_045303) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
   enable_extension "plpgsql"
@@ -94,8 +93,8 @@ ActiveRecord::Schema.define(version: 2021_08_24_045303) do
   create_table "identities", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
     t.string "uid", null: false
     t.integer "provider"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.string "name"
     t.string "avatar"
     t.string "url"
